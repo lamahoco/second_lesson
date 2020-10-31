6a497d991635abc32448711656b254c2e7bc890f
Alexander Timonenkov <nightblaze1@gmail.com>
2020-10-31 19:23:45 +0300

    проект

--- second_lesson/src/main/kotlin/main.kt
+++ second_lesson/src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
-    menu()
+    menu(name = "миша")
+    menu(name = "миша")
 }
 
 fun swap()  {
@@ -11,15 +12,22 @@ fun swap()  {
     b = v
 
     println("a=$a b=$b") // a=20 b=10
+
 }
 
-fun menu() {
+fun menu(name: String) {
+    println("привет, $name !!!")
     println("+------------------------------+")
     println("|             меню             |")
     println("|      1.поменять местами      |")
     println("|           0.выход            |")
     println("+------------------------------+")
+    println("введите число")
 }
 
+fun sum(a: Int, b: Int): Int {
+    var c:Int = a + b
+    return c
+}
 
 
